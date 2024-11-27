b4ff77017d1f4fe396d883164fdfc4908c741da6
xa81 <poisonkid@gmail.com>
2024-11-28 02:09:22 +0300

    Version 1.17.2 - performans , premium domain query

--- coremio/modules/Registrars/DomainNameAPI/config.php
+++ coremio/modules/Registrars/DomainNameAPI/config.php
@@ -3,7 +3,7 @@
 return [
     'meta'     => [
         'name'    => 'DomainNameAPI',
-        'version' => '1.17.0',
+        'version' => '1.17.2',
         'logo'    => 'logo.png',
     ],
     'settings' => [
