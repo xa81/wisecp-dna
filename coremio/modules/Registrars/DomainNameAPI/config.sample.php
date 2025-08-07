fb77c7d63ef9cc32c162df36b0ce8db91b78fe67
xa81 <poisonkid@gmail.com>
2025-08-07 12:17:07 +0300

    Bump version to 1.18.6 in DomainNameAPI

--- coremio/modules/Registrars/DomainNameAPI/config.sample.php
+++ coremio/modules/Registrars/DomainNameAPI/config.sample.php
@@ -3,7 +3,7 @@
 return [
     'meta'     => [
         'name'    => 'DomainNameAPI',
-        'version' => '1.18.5',
+        'version' => '1.18.6',
         'logo'    => 'logo.png',
     ],
     'settings' => [
